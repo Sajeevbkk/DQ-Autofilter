23ba9de22d2b29f8df9cf14fb3dac4a04e472702
Sajeevbkk <96622442+Sajeevbkk@users.noreply.github.com>
2024-05-01 10:14:55 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -28,7 +28,7 @@ async def answer(bot, query):
     if not await inline_users(query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='okDa',
+                           switch_pm_text='Bot Premium Required',
                            switch_pm_parameter="hehe")
         return
 
